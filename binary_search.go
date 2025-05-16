2743144e3ab85310f8483a1b3595ef31f0b95c73
Murodxon N. Qodirxonov <m_qodirxonov@uzinfocom.local>
2025-05-16 16:52:02 +0500

    optimized binary_search.go file

--- binary_search.go
+++ binary_search.go
@@ -4,32 +4,29 @@ import "fmt"
 
 func main() {
 	list := []int{1, 9, 13, 25, 34, 99, 1022, 1234, 1678}
+	needle := 34
 
-	index, found := binary_search(list, 343)
-
-	if found {
-		fmt.Println(index)
+	if index, found := binarySearch(list, needle); found {
+		fmt.Printf("Index: %d\n", index)
 	} else {
-		fmt.Println("Ma'lumot topilmadi")
+		fmt.Println("Ma'lumot topilmadi.")
 	}
 }
 
-func binary_search(haystack []int, needle int) (int, bool) {
-	low := 0                  // pastki ko'rsatgich o dan boshlanadi
-	high := len(haystack) - 1 // yuqori ko'rsatgich ro'yhatning oxirgi elementidan boshlanadi
+func binarySearch(haystack []int, needle int) (int, bool) {
+	low, high := 0, len(haystack)-1
 
-	for low <= high { // pastki va yuqori ko'rsatgich bitta qiymatgacha kelguncha loop bajarilishi kerak
-		middle := (low + high) / 2 // ro'yhatni yarmidan ajratib olamiz
+	for low <= high {
+		mid := (low + high) / 2
 
-		if haystack[middle] == needle {
-			return middle, true // agar needle middle ga teng bo'lsa demak natijani topdik !
-		}
-
-		if needle > haystack[middle] {
-			low = middle + 1 // agar needle middledan katta bo'lsa, pastki ko'rsatgichni yarmigacha suramiz, qolgani kerak emas
-		} else {
-			high = middle - 1 // // agar needle middledan katta bo'lsa, yuqori ko'rsatgichni yarmigacha pasaytiramiz
+		switch {
+		case haystack[mid] == needle:
+			return mid, true
+		case needle > haystack[mid]:
+			low = mid + 1
+		default:
+			high = mid - 1
 		}
 	}
-	return -1, false // ma'lumot topilmadi
+	return -1, false
 }
